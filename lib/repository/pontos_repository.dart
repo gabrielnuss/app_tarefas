72c740d509a341171e0c6f762f00d364430f698c
Gabriel <gabrielnuss40@gmail.com>
2023-11-16 21:48:37 -0300

    commit com alterações referente a rotina de pontuação

---
+++ lib/repository/pontos_repository.dart
@@ -0,0 +1,27 @@
+import 'package:app_tarefas/repository/database.dart';
+
+class PontosRepository {
+  Future<int> selectPontos() async {
+    var db = await DataBaseSQLite().getDataBase();
+
+    var result = await db.rawQuery("SELECT ponto FROM pontos");
+    if (result.isEmpty) {
+      return 0;
+    } else {
+      var mapResult = result[0];
+      return int.parse(mapResult["ponto"].toString());
+    }
+  }
+
+  Future<void> somarPonto() async {
+    var db = await DataBaseSQLite().getDataBase();
+    var result = await db.rawQuery("SELECT ponto FROM pontos");
+    if (result.isEmpty) {
+      await db.rawInsert("INSERT INTO pontos (ponto) values(?)", [10]);
+    } else {
+      var mapResult = result[0];
+      int ponto = int.parse(mapResult["ponto"].toString());
+      await db.rawUpdate("UPDATE pontos SET ponto = ?", [ponto + 10]);
+    }
+  }
+}
